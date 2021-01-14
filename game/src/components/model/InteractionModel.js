01f8796fab21f658953334c39fe65c71b2b4073b
Alexandre Carvalho <alexandremcarv@gmail.com>
2021-01-14 15:08:28 -0400

    Implementation of player turn

---
+++ game/src/components/model/InteractionModel.js
@@ -0,0 +1,34 @@
+export default class InteractionModel {
+  _currentTurn;
+  _playerTurn;
+  _selectedPiece;
+  
+  constructor(players) {
+    this._players = players;
+    this._playerTurn = 0;
+  }
+
+  get playerTurn() {
+    return this._players[this._playerTurn];
+  }
+
+  changePlayerTurn() {
+    this._playerTurn = (this._playerTurn + 1) % 2;
+  }
+
+  get selectedPiece() {
+    return this._selectedPiece;
+  }
+
+  set selectedPiece(piece)  {
+    this._selectedPiece = piece;
+  }
+
+  get currentTurn() {
+    return this._currentTurn;
+  }
+
+  incrementTurn() {
+    this._currentTurn += 1;
+  }
+}
