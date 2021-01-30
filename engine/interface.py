618e76683b1debeec83fa32662104bff656ba799
Ben Myles <ben.myles4@gmail.com>
2021-01-30 09:58:00 -0400

    Create interface

---
+++ engine/interface.py
@@ -0,0 +1,17 @@
+class UHP_Interface:
+    def info():
+        pass
+    def newgame():
+        pass
+    def play():
+        pass
+    def pass():
+        pass
+    def validmoves():
+        pass
+    def bestmove():
+        pass
+    def undo():
+        pass
+    def options():
+        pass
