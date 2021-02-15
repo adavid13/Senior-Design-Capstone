30d37b43b20df07dc6a9c0c91f4c0ce3678fd676
tye-shutty <shuttyt@gmail.com>
2021-02-15 17:11:25 -0400

    Change isBeetled to beetleOnTop

--- engine/AntPiece.py
+++ engine/AntPiece.py
@@ -4,7 +4,7 @@ class AntPiece:
         self.pieceNum = pieceNum
         self.coordinates = None
         self.id = "{}A{}".format(self.colour, self.pieceNum)
-        self.isBeetled = False
+        self.beetleOnTop = None
 
     def __eq__(self, other):
         return self.id == other.id
