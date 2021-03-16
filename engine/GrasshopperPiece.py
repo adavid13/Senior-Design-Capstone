ed36671baedd8c36522d4475b211d8b1b93b312d
Ben Myles <ben.myles4@gmail.com>
2021-03-15 23:21:19 -0300

    Create Grasshopper.validMoves

--- engine/GrasshopperPiece.py
+++ engine/GrasshopperPiece.py
@@ -14,3 +14,33 @@ class GrasshopperPiece:
 
     def __str__(self):
         return self.id
+
+    def validMoves(self, model):
+        cpModel = model.deepCopy()
+        # Remove self from the copied board
+        cpModel.board.Board[self.coordinates[0]][self.coordinates[1]] = None
+        cpModel.board.pieces.remove(self)
+        if (not cpModel.board.isHiveConnected()):
+            # Hive is not connected without this piece therefore it cannot be moved
+            return []
+
+        neighbours = [[-2, 0], [-1, -1], [1, -1], [2, 0], [1, 1], [-1, 1]]
+        x = self.coordinates[0]
+        y = self.coordinates[1]
+        moves = []
+
+        for i in range(len(neighbours)):
+            dx,dy = neighbours[i]
+            dist = 0
+            x = self.coordinates[0]
+            y = self.coordinates[1]
+            nextPiece = cpModel.board.Board[x+dx][y+dy]
+            while nextPiece is not None:
+                x += dx
+                y += dy
+                dist += 1
+                nextPiece = cpModel.board.Board[x+dx][y+dy]
+            if dist > 0:
+                moves.append([x+dx,y+dy])
+
+        return moves
