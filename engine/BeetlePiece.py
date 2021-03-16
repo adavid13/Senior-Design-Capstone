6c0baf156ed659634fe52ee86e6bb591fc7bf008
Ben Myles <ben.myles4@gmail.com>
2021-03-16 10:42:31 -0300

    Fix gap jumping bug for beetle Fixed using common neighbours check to make sure the new location is neighbouring at least one of the beetle's previous neighbours.

--- engine/BeetlePiece.py
+++ engine/BeetlePiece.py
@@ -7,16 +7,17 @@ class BeetlePiece:
         self.beetleOnTop = None
         self.beetling = None
 
+# TODO: make beetle's neighbours include the piece it's beetling
     def validMoves(self, model):
-
         if self.beetleOnTop is not None:
             return []
         
+        copyGameModel = model.deepCopy()
+
         moves = []
         neighbours = [[-2, 0], [-1, -1], [1, -1], [2, 0], [1, 1], [-1, 1]]
+        neighbourPieces = copyGameModel.board.getNeighbours(piece=self)
 
-
-        copyGameModel = model.deepCopy()
         # Remove the piece from the board copy if its not beetling
         # Beetles that are on top of another piece can't break the hive by moving. At all
         if self.beetling is None:
@@ -35,17 +36,15 @@ class BeetlePiece:
                 x2, y2 = neighbours[(i+1) % 6]
                 # If there is no sliding rule infringement that blocks the piece from moving
                 if (copyGameModel.board.Board[x+x1][y+y1]) is None or (copyGameModel.board.Board[x+x2][y+y2] is None) or (copyGameModel.board.Board[x+dx][y+dy] is not None):
-                    for j in range(len(neighbours)):
-
-                        xj, yj = neighbours[j][0], neighbours[j][1]
-                        if copyGameModel.board.Board[x+dx+xj][y+dy+yj] is not None:
-                            moves.append([x+dx, y+dy])
-                            break
-
+                    # If current location and next location have any common neighbours (including the piece that may be being beetled)
+                    # Note: Fixes gap jumping bug
+                    newNeighbourPieces = copyGameModel.board.getNeighbours(coords=[x+dx,y+dy])
+                    if copyGameModel.board.Board[x+dx][y+dy] is not None:
+                        newNeighbourPieces.append(copyGameModel.board.Board[x+dx][y+dy])
+                    commonNeighbours = [p for p in neighbourPieces if p in newNeighbourPieces]
+                    if commonNeighbours:
+                        moves.append([x+dx, y+dy])
             return moves
-
-
-
 
         elif self.beetling is not None:
             # For beetles that are on top of a piece, there is no invalid
@@ -53,7 +52,6 @@ class BeetlePiece:
             x, y = self.coordinates[0], self.coordinates[1]
             moves = [[x+neighbour[0], y+neighbour[1]] for neighbour in neighbours]
             return moves
-
 
     def __eq__(self, other):
         return self.id == other.id
