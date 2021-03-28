f3b1b507bf23c3e70cd164eb27cec965cc47c17b
Jakob-B-R <jakob.b.roberts@gmail.com>
2021-03-27 22:01:20 -0300

    set of valid moves done

--- ChexyAgentServer/engine/ArtificialAgent.py
+++ ChexyAgentServer/engine/ArtificialAgent.py
@@ -7,8 +7,11 @@ class ArtificialAgent:
 
     def setOfValidMoves(self, gamemodel):
         validMoves = gamemodel.validMoves()[0:-1]
-        moveList = [self.moveStringToIndexes(gamemodel, p) for p in validMoves.split(";") if p != '']
-        return moveList
+        moveList1 = [p for p in validMoves.split(";") if p != '']
+        moveList = [self.moveStringToStandardString(gamemodel, p) for p in moveList1]
+        setMoveList = []
+        [setMoveList.append(x) for x in moveList if x not in setMoveList]
+        return setMoveList
         """
         valid moves contains a lot of redundant data.
         This should be reduced to a small subset of values to reduce the branching factor
@@ -16,45 +19,51 @@ class ArtificialAgent:
 
         pass
 
-    def moveStringToIndexes(self, model, movestring):
-        neighbours = [[-2, 0], [-1, -1], [1, -1], [2, 0], [1, 1], [-1, 1]]
-        symbols = ["{} {}-", "{} {}\\", "{} /{}", "{} -{}", "{} \\{}", "{} {}/"]
-        p1Coord = []
-        p2Coord = []
-        moveStringSplit = movestring.split(" ")
-        p1 = model.board.getPieceFromString(moveStringSplit[0])
-        if p1 is None:
-            p1 = moveStringSplit[0]
-            pstr = p1
-            p1Coord = pstr
-        else:
-            pstr = p1.id
-            p1Coord = p1.coordinates
-        
-        relPiece = moveStringSplit[1]
-        i = None
-        p2 = None
-        if relPiece[0] == "-":
-            i = 3
-            p2 = model.board.getPieceFromString(relPiece[1:])
-        elif relPiece[0] == "/":
-            i = 2
-            p2 = model.board.getPieceFromString(relPiece[1:])
-        elif relPiece[0] == "\\":
-            i = 4
-            p2 = model.board.getPieceFromString(relPiece[1:])
-        elif relPiece[3] == "-":
-            i = 0
-            p2 = model.board.getPieceFromString(relPiece[:-1])
-        elif relPiece[3] == "/":
-            i = 5
-            p2 = model.board.getPieceFromString(relPiece[:-1])
-        elif relPiece[3] == "\\":
-            i = 1
-            p2 = model.board.getPieceFromString(relPiece[:-1])
+    def moveStringToStandardString(self, model, movestring):
 
-        p2Coord = (p2.coordinates[0]+neighbours[i][0], p2.coordinates[1]+neighbours[i][1])
-        return [p1Coord, p2Coord]
+        neighbours = [[-2, 0], [-1, -1], [1, -1], [2, 0], [1, 1], [-1, 1]]
+        symbols =         ["{} {}-", "{} {}\\", "{} /{}", "{} -{}", "{} \\{}", "{} {}/"]
+        oppositeSymbols = ["{} {}-", "{} \\{}", "{} {}/", "{} -{}", "{} {}\\", "{} /{}"]
+        if len(movestring) <= 3:
+            return movestring
+        movingPiece, relPiece = movestring.split(" ")
+        if len(relPiece) == 3:
+            p2 = relPiece
+            return movestring
+
+        elif relPiece[3] == "-":
+            p2 = relPiece[0:3]
+            i = 0
+        
+        elif relPiece[3] == "\\":
+            p2 = relPiece[0:3]
+            i = 1
+
+        elif relPiece[0] == "/":
+            p2 = relPiece[1:]
+            i = 2
+
+        elif relPiece[0] == "-":
+            p2 = relPiece[1:]
+            i = 3
+
+        elif relPiece[0] == "\\":
+            p2 = relPiece[1:]
+            i = 4
+
+        elif relPiece[3] == "/":
+            p2 = relPiece[0:3]
+            i = 5
+
+        relCoords = [model.board.getPieceFromString(p2).coordinates[0], model.board.getPieceFromString(p2).coordinates[1]]
+        relCoords[0] -= neighbours[i][0]
+        relCoords[1] -= neighbours[i][1]
+        for j in range(len(neighbours)):
+            new_x, new_y = relCoords[0] + neighbours[j][0], relCoords[1] + neighbours[j][1]
+            if model.board.Board[new_x][new_y] is not None:
+                return symbols[j].format(movingPiece, model.board.Board[new_x][new_y])
+        raise Exception("*Skyrim Guard voice* You shouldn't be here")
+        
 
     def bestMove(self, gameModel, difficulty=0, maxTime=None, maxDepth=None):
         if gameModel.turnColor == "Black" and difficulty > 0:
